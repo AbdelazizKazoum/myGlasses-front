9a2a65a0802e1bd7564e8375db97a8735617ecce
Abdelaziz_kazoum <abdukazoum@gmail.com>
2025-03-19 13:15:41 +0000

    fix margin

--- src/components/dashboard/SettingsPage/FournisseurTab.jsx
+++ src/components/dashboard/SettingsPage/FournisseurTab.jsx
@@ -14,7 +14,7 @@ const ConfirmDeleteModal = ({ isOpen, onClose, onConfirm }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center z-50">
+    <div className="fixed inset-0 bg-gray-600 m-0 bg-opacity-50 flex justify-center items-center z-50">
       <div className="bg-white rounded-lg p-6 w-96">
         <h3 className="text-lg font-semibold text-gray-800">Confirmation</h3>
         <p className="text-sm text-gray-600 mt-2">
