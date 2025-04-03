02755e90242813c86387700293966b4fd2a6b2db
Abdelaziz_kazoum <abdukazoum@gmail.com>
2025-04-03 10:55:01 +0000

    fix redirect bug

--- src/components/ProtectedRoute/GuestOnly.jsx
+++ src/components/ProtectedRoute/GuestOnly.jsx
@@ -1,13 +1,23 @@
 import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 const GuestOnly = (props) => {
   const { user } = useSelector((state) => state.auth);
 
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const redirectPath = params.get("redirect");
+
   if (user) {
     if (user.role === "admin") {
+      if (redirectPath) {
+        return <Navigate to={redirectPath} />;
+      }
       return <Navigate to="/admin" />;
     } else {
+      if (redirectPath) {
+        return <Navigate to={redirectPath} />;
+      }
       return <Navigate to="/" />;
     }
   }
